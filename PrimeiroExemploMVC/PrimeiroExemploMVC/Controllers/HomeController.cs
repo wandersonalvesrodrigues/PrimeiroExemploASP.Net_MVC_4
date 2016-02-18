f0577d008fa1f369c3f131419375519ec453faaa
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-02-18 21:47:26 -0200

    Criação do primeiro exemplo.

---
+++ PrimeiroExemploMVC/PrimeiroExemploMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeiroExemploMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/Index
+        //Esse método será chamado sempre no caminho padrão, que foi definido na Rota
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Criar um novo action para representar o sobre do site
+        public ActionResult About() 
+        {
+            return View();
+        }
+
+    }
+}
